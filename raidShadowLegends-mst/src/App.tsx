9947aeb5766d999c33134b8821999065cb2ed920
SimeonNedyalkov <simeon_sg@abv.bg>
2024-11-22 12:01:48 +0200

    app setup

---
+++ raidShadowLegends-mst/src/App.tsx
@@ -0,0 +1,11 @@
+import Home from "./components/Home";
+import "./styles/base.css";
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ raidShadowLegends-mst/src/components/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="home flex justify-center items-center">
+      <div>Hello World</div>
+    </div>
+  );
+}
